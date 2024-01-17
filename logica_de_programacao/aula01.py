52bb0c89e37802a1808a6ad7c5f479f314387d1f
Gustavo Pinto <96655520+gustavohenriquepinto@users.noreply.github.com>
2024-01-17 15:09:54 -0300

    Lógica de programação

---
+++ logica_de_programacao/aula01.py
@@ -0,0 +1,12 @@
+# <-- Usado para escrever comentários
+
+"""
+Isso não é um comentário, é um docstring.
+"""
+
+'''
+Isso não é um comentário, é um docstring.
+'''
+
+print('Olá, mundo') # Escreva "Olá, mundo!"
+print(123) # Escreva 123
---
+++ logica_de_programacao/aula03.py
@@ -0,0 +1,15 @@
+# Não se pode utilizar o mesmo tipo de aspas dentro de uma mesma string.
+
+# Aspas simples
+print('Gustavo " Henrique')
+
+# Aspas duplas
+print("Gustavo ' Henrique")
+
+# Escape
+print('Gustavo \\ \" Henrique')
+
+# r
+print(r"Gustavo \\ \" \' Henrique")
+
+print('Python é uma linguagem de tipagem forte e dinâmica.')
