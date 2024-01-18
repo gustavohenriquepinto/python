c9864a5d15c5a892225566638e85bdf233950968
Gustavo Pinto <96655520+gustavohenriquepinto@users.noreply.github.com>
2024-01-17 21:48:43 -0300

    Variáveis e operadores

--- logica_de_programacao/aula03_variaveis.py
+++ logica_de_programacao/aula03_variaveis.py
@@ -1,10 +1,24 @@
 # Variáveis são usadas para salvar dados na memória do computador. Para atribuir um valor a uma variável, é necessário utilizar '=', o sinal de atribuição.
 # Variáveis devem ser iniciadas com letras minúsculas, podendo conter letras, números e underline '_'
 
-nome = 'Gustavo Henrique'
-soma_2_mais_2 = 2 + 2
-int_um = bool('1')
+# nome = 'Gustavo Henrique'
+# soma_2_mais_2 = 2 + 2
+# int_um = bool('1')
 
-nome = 'Gustavo'
-idade = 30
+# nome = 'Gustavo'
+# idade = 30
+# maior_de_idade = idade >= 18
+
+nome = 'Gustavo Henrique'
+sobrenome = 'da Silva Pinto'
+idade = 19
+ano_nascimento = 2024 - idade
 maior_de_idade = idade >= 18
+altura_metros = 1.73
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('Maior de idade:', maior_de_idade)
+print('Altura em metros:', altura_metros)
---
+++ logica_de_programacao/aula04_operadores.py
@@ -0,0 +1,25 @@
+# n1 = 3
+# n2 = 2
+
+# print('Adição (+):', n1+n2)
+# print('Subtração (-):', n1-n2)
+# print('Multiplicação (*):', n1*n2)
+# print('Divisão (/):', n1/n2)
+# print('Divisão inteira (//):', n1//n2)
+# print('Exponenciação (**):', n1**n2)
+# print('Resto da divisão (%):', n1%n2)
+
+# a1 = 'A'
+# a2 = 'B'
+# n3 = 5
+
+# print('Concatenação (+):', a1+a2)
+# print('Repetição (*):', a1*n3)
+
+print('Precedência de operadores:', '1. ()', '2. **', '3. * / // %', '4. + -', sep="\n")
+
+nome = 'Gustavo'
+altura  = 1.73
+peso = 83.5
+
+print('Olá, ' + nome + ', seu peso é de', peso, 'e sua altura é de', altura, '. Seu IMC é de', peso/altura**2)
