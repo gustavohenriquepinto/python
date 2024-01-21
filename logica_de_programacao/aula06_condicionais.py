7edc49a715e7d2ccdf5bba565d7d4dec4162cb5a
Gustavo Pinto <96655520+gustavohenriquepinto@users.noreply.github.com>
2024-01-21 10:50:07 -0300

    operadores lógicos

--- logica_de_programacao/aula06_condicionais.py
+++ logica_de_programacao/aula06_condicionais.py
@@ -1,12 +1,42 @@
 # if / elif / else
 
-nomeUsuario = input('Qual seu nome? ')
-senhaUsuario = input('Digite sua senha: ')
+# nomeUsuario = input('Qual seu nome? ')
+# senhaUsuario = input('Digite sua senha: ')
 
-if senhaUsuario == '':
-    print(f'{nomeUsuario}, você não preencheu o campo de senha.')
-elif senhaUsuario != 'abc123':
-    print('Digite sua senha novamente, {}.'.format(nomeUsuario))
-else:
-    print(f'Parabéns, {nomeUsuario}, você entrou no sistema.')
+# if senhaUsuario == '':
+#     print(f'{nomeUsuario}, você não preencheu o campo de senha.')
+# elif senhaUsuario != 'abc123':
+#     print('Digite sua senha novamente, {}.'.format(nomeUsuario))
+# else:
+#     print(f'Parabéns, {nomeUsuario}, você entrou no sistema.')
 
+# Comparadores
+
+# n1 = 2
+# n2 = 3
+
+# igual = n1 == n2
+# diferente = n1 != n2
+# maior = n1 > n2
+# maior_ou_igual = n1 >= n2
+# menor = n1 < n2
+# menor_ou_igual = n1 <= n2
+
+# print(igual)
+# print(diferente)
+# print(maior)
+# print(maior_ou_igual)
+# print(menor)
+# print(menor_ou_igual)
+
+# primeiro_valor = input('Digite o primeiro valor: ')
+# segundo_valor = input('Digite o segundo valor: ')
+
+# if primeiro_valor > segundo_valor:
+#     print(f'{primeiro_valor=} é maior do que {segundo_valor=}.')
+# elif primeiro_valor == segundo_valor:
+#     print(f'{primeiro_valor=} é igual a {segundo_valor=}.')
+# else:
+#     print(f'{segundo_valor=} é maior do que {primeiro_valor=}.')
+
+# print(f'{primeiro_valor=} é maior do que {segundo_valor=}.' if primeiro_valor > segundo_valor else f'{primeiro_valor=} é igual a {segundo_valor=}.' if primeiro_valor == segundo_valor else f'{segundo_valor=} é maior do que {primeiro_valor=}.')
