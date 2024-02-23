bf6beaae8642099684d0de577de0a9bb654d147d
Gustavo Pinto <96655520+gustavohenriquepinto@users.noreply.github.com>
2024-02-22 21:49:02 -0300

    Gerador cpf

--- logica_de_programacao/aula16_validador_cpf.py
+++ logica_de_programacao/aula16_validador_cpf.py
@@ -1,26 +1,34 @@
 
-# 754.233.956-74
-# 754233956
+import re
+import random
 
-# 70+45+32+14+18+15+36+15+12 = 257
-# 257 * 10 = 2570
-# 2570 % 11 = 7
+numero_cpfs = 10
+while numero_cpfs > 0:
+    texto_numero = ''
+    
+    # texto_usuario = re.sub(
+    #     r'[^0-9]',
+    #     '',
+    #     input('Digite  os nove primeiros números do seu CPF: '),
+    # )
 
+    # if (not texto_usuario.isnumeric()) or int(texto_usuario) < 100000000:
+    #     if texto_usuario.upper().startswith('S'):
+    #         print('Tchau!')
+    #         break
 
-while True:
-    texto_usuario = input('Digite  os nove primeiros números do seu CPF: ')
-
-    if not texto_usuario.isnumeric():
-        if texto_usuario.upper().startswith('S'):
-            print('Tchau!')
-            break
-
-        print('Digite apenas números!')
-        continue
+    #     print('Digite apenas os nove primeiros números!')
+    #     continue
+    # else:
+    #     texto_numero = texto_usuario
+    
+    for i in range(9):
+        texto_numero += str(random.randint(0, 9))
+    
 
     contador_regressivo = 10
     soma_digitos = 0
-    for digito in texto_usuario:
+    for digito in texto_numero:
         soma_digitos += int(digito) * contador_regressivo
         contador_regressivo -= 1
 
@@ -29,11 +37,17 @@ while True:
 
     contador_regressivo = 11
     soma_digitos = 0
-    for digito in texto_usuario + str(validador_1):
+    for digito in texto_numero + str(validador_1):
         soma_digitos += int(digito) * contador_regressivo
         contador_regressivo -= 1
     
     validador_2 = (soma_digitos * 10) % 11
     validador_2 = validador_2 if validador_2 < 10 else 0
 
-    print(f'Os dígitos validadores são {validador_1}{validador_2}.')
+    # print(f'Os dígitos validadores são {validador_1}{validador_2}.')
+
+    print(texto_numero + str(validador_1) + str(validador_2))
+    numero_cpfs -= 1
+
+
+
