01cc735b5451142b565be0927ce92452acfa7d71
Gustavo Pinto <96655520+gustavohenriquepinto@users.noreply.github.com>
2024-03-05 09:38:56 -0300

    Dicionários

---
+++ logica_de_programacao/aula18_dicionarios.py
@@ -0,0 +1,25 @@
+# pessoa = {
+#     'nome' : 'Gustavo',
+#     15 : 'Pinto',
+#     True : 19,
+#     'Tupla' : ('Gustavo', 19),
+# }
+
+# print(pessoa)
+# print(type(pessoa))
+
+# caderno = dict(
+#     capa = 'Azul',
+#     folhas = 96,
+#     de_arame = True,
+# )
+
+# print(caderno)
+# print(type(caderno))
+
+produto = {}
+
+produto['tipo'] = 'Biscoito'
+
+
+print(produto)
