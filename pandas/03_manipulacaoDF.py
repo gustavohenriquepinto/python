d07485acb56ad8fc1f9c593e2f0bfc5a7f4a16ab
Gustavo Pinto <ggustavohenriquepinto45@gmail.com>
2024-09-07 01:13:33 +0000

    Update 03_manipulacaoDF.py

--- pandas/03_manipulacaoDF.py
+++ pandas/03_manipulacaoDF.py
@@ -2,5 +2,21 @@ import pandas as pd
 
 data = pd.read_csv('~/dev/python/pandas/datasets/GasPricesinBrazil_2004-2019.csv', sep = ';')
 
-#print(data['ESTADO'])
+# print(data['ESTADO'])
+# print(data.iloc[0])
+#
+#product_reference = data['PRODUTO'] 
+#product_copy = data['PRODUTO'].copy()
+#
+#data['PRODUTO'] = 'Hello, world!'
+#
+#print(data['PRODUTO'])
+#print(product_reference)
+#print(product_copy)
 
+#data_rows, data_cols = data.shape
+#
+#produtos = [f'Produto {i}' for i in range(data_rows)]
+#print(produtos)
+
+print(list(data.index))
